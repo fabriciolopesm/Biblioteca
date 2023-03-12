5990d430b8de850dbb2a742911f6533ad1960757
Fabricio Lopes <fabriciolopesm@gmail.com>
2023-03-12 18:03:20 -0300

    .

--- livro/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Livros, Categoria, Emprestimos
-
-admin.site.register(Categoria)
-admin.site.register(Livros)
-admin.site.register(Emprestimos)
-
-
